27153fed359d6b26dae162191720d90b7066c860
DmitriyVladimirovich <150802655+DmitriyVladimirovich@users.noreply.github.com>
2023-12-06 16:01:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
+from pynput import keyboard
 from map import Map
 import time
 import os
+from helicopter import Helicopter as Hel
+from clouds import Clouds
 
-TICK_SLEEP=50
+
+TICK_SLEEP=1
+TREE_UPDATE=50
 FIRE_UPDATE=100
+CLOUDS_UPDATE=300
 MAP_W,MAP_H=20,10
 
 field = Map(MAP_W,MAP_H)
-field.generate_forest(4,10)
-field.generate_river(10)
-field.generate_river(15)
-field.add_fire()
-field.print_map()
+hel=Hel(MAP_W,MAP_H)
+clouds=Clouds(MAP_W,MAP_H)
 
 tick=1
 
+MOVES={'w':(-1,0), 'd':(0,1), 's':(1,0), 'a':(0,-1)}
+def process_key(key):
+    global hel
+    c=key.char.lower()
+    if c in MOVES.keys():
+        dx,dy=MOVES[c][0], MOVES[c][1]
+        hel.move(dx,dy)
+    
+listener = keyboard.Listener(
+    on_press=None,
+    on_release=on_release)
+listener.start()
+
 while True:
     o.system('cls')
-	print('Tick:',tick)
-    field.print_map()
+	field.process_helicopter(hel,clouds)
+    hel.print_stats()
+    field.print_map(hel,clouds)
+    print('Tick:',tick)
 	tick+=1
 	time.sleep(TICK_SLEEP)
     if (tick%TREE_UPDATE==0):
         field.generate_tree()
     if (tick%FIRE_UPDATE==0):
         field.update_fires()
+    if (tick%CLOUDS_UPDATE==0):
+        clouds.update()
--- map.py
+++ map.py
@@ -1,4 +1,5 @@
 from utils import *
+import os
 # 0-поле
 # 1-дерево
 # 2-река
@@ -6,10 +7,24 @@ from utils import *
 # 4-магаз
 # 5-пожар
 
-CELL_TYPES=''
+CELL_TYPES='gtwrsf'
+TREE_BONUS=10
+UPGRADE_COST=500
+LIFE_COST=1000
 
 class Map:
-	def generate_rivers(self,l):
+	
+    def __init__(self,w,h):
+		self.w=w
+		self.h=h
+		self.cells=[[0 for i in range(w)] for j in range(h)]
+        self.generate_forest(3,10)
+        self.generate_river(10)
+        self.generate_river(15)
+        self.generate_upgrade_shop()
+        self.generate_hospital()
+    
+    def generate_rivers(self,l):
         rc=randcell(self.w, self.h)
         rx,ry=rc[0],rc[1]
         self.cells[rx][ry]=2
@@ -24,7 +39,7 @@ class Map:
     def generate_tree(self):
         c=randcell(self.w,self.h)
         cx,cy=c[0],c[1]
-        if (self.checkbounds(cx,cy) and selfcells[cx][cy]==0)
+        if (selfcells[cx][cy]==0)
             self.cells[cx][cy]=1
         
 	def generate_forest(self,r,mxr):
@@ -33,12 +48,32 @@ class Map:
                 if randbool(r,mxr):
                     self.cells[ri][ci]=1
 	
-	def print_map(self):
+    def generate_upgrade_shop(self):
+        c=randcell(self.w,self.h)
+        cx,cy=c[0],c[1]
+        self.cells[cx][cy]=4
+        
+    def generate_hospital(self):
+        c=randcell(self.w,self.h)
+        cx,cy=c[0],c[1]
+        if elf.cells[cx][cy]!=4
+            self.cells[cx][cy]=3
+        else:
+            self.generate_hospital()
+    
+	def print_map(self,helc,clouds):
 		print('*'*(self.w+2))
-		for row in self.cells:
+		for ri in range(self.h):
 			print('*', end='')
-			for cell in row:
-				if  (cell>=0 and cell<len(CELL_TYPES)):
+			for ci in range(self.w):
+                cell=self.cells[ri][ci]
+                if (clouds.cells[ri],[ci]==1):
+                    print('c',end='')
+                elif (clouds.cells[ri],[ci]==2):
+                    print('e',end='')
+                elif (helc.x==ri and helc.y==ci):
+                    print('h',end='')
+				elif (cell>=0 and cell<len(CELL_TYPES)):
 					print(CELL_TYPES[cell], end='')
 			print('*')
 		print('*'*(self.w+2))
@@ -63,7 +98,24 @@ class Map:
 			return False
 		return True
 	
-	def __init__(self,w,h):
-		self.w=w
-		self.h=h
-		self.cells=[[0 for i in range(w)] for j in range(h)]
+    def process_helicopter(self,hel,clouds):
+        с=self.cells[hel.x][hel.y]
+        d=clouds.cells[hel.x][hel.y]
+        if (с==2)
+            hel.tank=hel.mxtank
+        if (c==5 and hel.tank>0):
+            hel.tank-=1
+            hel.score+=TREE_BONUS
+            c=1
+        if (c==4 and hel.score>=UPGRADE_COST):
+            hel.mxtank+=1
+            hel.score-=UPGRADE_COST
+        if (c==3 and hel.score>=LIFE_COST):
+            hel.lives+=1000
+            hel.score-=LIFE_COST
+        if d==2:
+            hel.lives-=1
+            if hel.lives==0:
+                print('Game over, score:',hel.score)
+                os.system('cls')
+                exit(0)
